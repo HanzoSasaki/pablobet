cfdf17daaa14cf8c54666c1799396db1219309fe
erick barbosa <erickbsasaki@gmail.com>
2025-04-09 11:41:12 -0300

    new

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function verificarResultado() {
   } else {
     // Derrota: incrementa a contagem de perdas e exibe quantas vezes perdeu
     losses++;
-    resultDiv.textContent = `Se fudeu : :${losses} vezes 😬`;
+    resultDiv.textContent = `Se fudeu :${losses} vezes 😬`;
   }
   
   // Atualiza os stats
